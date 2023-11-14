68e7dfa1607e02aeabf2c948b3734c5a190b3eef
OTTOMAN12990 <9014996@student.zadkine.nl>
2023-11-14 13:07:05 +0100

    update Obsticle run

--- WEEK ^4^/Obstacle Run/Opdracht 11 - Obstacle Run/script.js
+++ WEEK ^4^/Obstacle Run/Opdracht 11 - Obstacle Run/script.js
@@ -1,59 +1,66 @@
 const dinosaur = document.querySelector('.dinosaur');
-const obstacle = document.querySelector('.obstacle');
 const gameOverText = document.querySelector('.game-over');
-
 let isJumping = false;
 let isGameOver = false;
+let score = 0;
+
 document.addEventListener('keydown', function (event) {
     if (event.key === 'w' && !isJumping && !isGameOver) {
-        isJumping = true;
-        dinosaur.style.animation = 'jump 0.8s';
-
-        setTimeout(() => {
-            dinosaur.style.animation = 'none';
-            isJumping = false;
-        }, 800);
+        jump();
     }
 });
+
+function jump() {
+    isJumping = true;
+    dinosaur.style.animation = 'jump 0.8s';
+
+    setTimeout(() => {
+        dinosaur.style.animation = 'none';
+        isJumping = false;
+    }, 800);
+}
+
 function checkCollision() {
     const dinosaurRect = dinosaur.getBoundingClientRect();
-    const obstacleRect = obstacle.getBoundingClientRect();
+    const obstacles = document.querySelectorAll('.obstacle');
 
-    if (
-        dinosaurRect.right > obstacleRect.left &&
-        dinosaurRect.left < obstacleRect.right &&
-        dinosaurRect.bottom > obstacleRect.top
-    ) {
-        gameIsOver();
-    }
+    obstacles.forEach((obstacle) => {
+        const obstacleRect = obstacle.getBoundingClientRect();
+
+        if (
+            dinosaurRect.right > obstacleRect.left &&
+            dinosaurRect.left < obstacleRect.right &&
+            dinosaurRect.bottom > obstacleRect.top
+        ) {
+            gameIsOver();
+        }
+    });
 }
 
 setInterval(checkCollision, 10);
+
 function gameIsOver() {
     isGameOver = true;
-    dinosaur.style.animation = 'none';
-    obstacle.style.animation = 'none';
+    document.querySelectorAll('.obstacle').forEach((obstacle) => {
+        obstacle.style.animation = 'none';
+    });
     gameOverText.style.display = 'block';
+    gameOverText.querySelector('h1').textContent = 'Game Over';
+    gameOverText.querySelector('p span').textContent = score;
 }
-// Voeg toe aan bestaande JavaScript-code
-
-let score = 0;
 
 function startGame() {
     document.querySelector('.start-button').style.display = 'none';
-    setInterval(() => {
-        createObstacle();
-    }, 1500);
+    generateObstacle();
 }
 
-function restartGame() {
-    isGameOver = false;
-    score = 0;
-    document.getElementById('score').textContent = score;
-    document.querySelector('.game-over').style.display = 'none';
-    document.querySelector('.dinosaur').style.animation = 'none';
-    document.querySelector('.obstacle').style.animation = 'none';
-    document.querySelector('.start-button').style.display = 'block';
+function generateObstacle() {
+    createObstacle();
+    setTimeout(() => {
+        if (!isGameOver) {
+            generateObstacle();
+        }
+    }, 1500);
 }
 
 function createObstacle() {
@@ -79,11 +86,15 @@ function increaseScore() {
     }
 }
 
-function gameIsOver() {
-    isGameOver = true;
+function restartGame() {
+    isGameOver = false;
+    score = 0;
+    document.getElementById('score').textContent = score;
+    document.querySelector('.game-over').style.display = 'none';
     document.querySelector('.dinosaur').style.animation = 'none';
-    document.querySelector('.obstacle').style.animation = 'none';
-    document.querySelector('.game-over').style.display = 'block';
-    document.querySelector('.game-over h1').textContent = 'Game Over';
-    document.querySelector('.game-over p span').textContent = score;
+    document.querySelectorAll('.obstacle').forEach((obstacle) => {
+        obstacle.remove();
+    });
+    document.querySelector('.start-button').style.display = 'block';
 }
+
